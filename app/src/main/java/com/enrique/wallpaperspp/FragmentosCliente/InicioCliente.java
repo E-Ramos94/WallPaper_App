25d61dbb26eab4814222ad4c12458a63265762c7
Enrique Ramos <kikeramos.1994@gmail.com>
2023-08-01 22:36:27 -0600

    Visualización de categorías del cliente implementado

--- app/src/main/java/com/enrique/wallpaperspp/FragmentosCliente/InicioCliente.java
+++ app/src/main/java/com/enrique/wallpaperspp/FragmentosCliente/InicioCliente.java
@@ -2,65 +2,92 @@ package com.enrique.wallpaperspp.FragmentosCliente;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.enrique.wallpaperspp.Categorias.Cat_Dispositivo.CategoriaD;
+import com.enrique.wallpaperspp.Categorias.Cat_Dispositivo.ViewHolderCD;
 import com.enrique.wallpaperspp.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InicioCliente#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class InicioCliente extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView recycleViewCategoriasD;
+    FirebaseDatabase firebaseDatabaseD;
+    DatabaseReference referenceD;
+    LinearLayoutManager linearLayoutManagerD;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public InicioCliente() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InicioCliente.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InicioCliente newInstance(String param1, String param2) {
-        InicioCliente fragment = new InicioCliente();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FirebaseRecyclerAdapter<CategoriaD, ViewHolderCD> firebaseRecyclerAdapterD;
+    FirebaseRecyclerOptions<CategoriaD> optionsD;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio_cliente, container, false);
+        View view = inflater.inflate(R.layout.fragment_inicio_cliente, container, false);
+
+        firebaseDatabaseD = FirebaseDatabase.getInstance();
+        referenceD = firebaseDatabaseD.getReference("CATEGORIAS_D");
+        linearLayoutManagerD = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+
+        recycleViewCategoriasD = view.findViewById(R.id.recycleViewCategoriasD);
+        recycleViewCategoriasD.setHasFixedSize(true);
+        recycleViewCategoriasD.setLayoutManager(linearLayoutManagerD);
+
+        VerCategoriasD();
+
+        return view;
+    }
+
+    private void VerCategoriasD() {
+        optionsD = new FirebaseRecyclerOptions.Builder<CategoriaD>().setQuery(referenceD, CategoriaD.class).build();
+        firebaseRecyclerAdapterD = new FirebaseRecyclerAdapter<CategoriaD, ViewHolderCD>(optionsD) {
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolderCD holder, int position, @NonNull CategoriaD model) {
+                holder.SeteoCategoriaD(
+                        getActivity(),
+                        model.getCategoria(),
+                        model.getImagen()
+                );
+            }
+
+            @NonNull
+            @Override
+            public ViewHolderCD onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                //INFLAR EL LAYOUT
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.categorias_dispositivo, parent, false);
+                ViewHolderCD viewHolderCD = new ViewHolderCD(itemView);
+
+                viewHolderCD.setOnClickListener(new ViewHolderCD.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                        String categoria = getItem(position).getCategoria();
+                        Toast.makeText(getActivity(), categoria, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                return viewHolderCD;
+            }
+        };
+        recycleViewCategoriasD.setAdapter(firebaseRecyclerAdapterD);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (firebaseRecyclerAdapterD != null) {
+            firebaseRecyclerAdapterD.startListening();
+        }
     }
 }
