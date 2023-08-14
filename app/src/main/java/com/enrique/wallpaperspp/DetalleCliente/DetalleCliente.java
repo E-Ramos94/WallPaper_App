7ec1aa1b805fcfbbac9f95aa8e2cf30c8433509a
Enrique Ramos <kikeramos.1994@gmail.com>
2023-08-14 15:46:48 -0600

    Función para descarga de imagen imeplementada

--- app/src/main/java/com/enrique/wallpaperspp/DetalleCliente/DetalleCliente.java
+++ app/src/main/java/com/enrique/wallpaperspp/DetalleCliente/DetalleCliente.java
@@ -3,7 +3,16 @@ package com.enrique.wallpaperspp.DetalleCliente;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +22,14 @@ import com.enrique.wallpaperspp.R;
 import com.github.clans.fab.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+
 public class DetalleCliente extends AppCompatActivity {
 
     ImageView ImagenDetalle;
@@ -20,6 +37,8 @@ public class DetalleCliente extends AppCompatActivity {
     TextView VistaDetalle;
 
     FloatingActionButton fabDescargar, fabCompatir, fabEstablecer;
+
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +77,7 @@ public class DetalleCliente extends AppCompatActivity {
         fabDescargar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(DetalleCliente.this, "Descargar", Toast.LENGTH_SHORT).show();
+                DescargarImagen_11();
             }
         });
 
@@ -76,6 +95,73 @@ public class DetalleCliente extends AppCompatActivity {
             }
         });
 
+        //EN CASO NO DESCARGUE LA IMAGEN
+        //StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        //StrictMode.setVmPolicy(builder.build());
+
+    }
+
+    private void DescargarImagen() {
+        //OBTENER MAPA DE BITS DE LA IMAGEN
+        bitmap = ((BitmapDrawable)ImagenDetalle.getDrawable()).getBitmap();
+        //OBTENER LA FECHA DE DESCARGA
+        String FechaDescarga = new SimpleDateFormat("'Fecha Descarga: ' yyyy_MM_dd 'Hora: ' HH:mm:ss",
+                Locale.getDefault()).format(System.currentTimeMillis());
+
+        //DEFINIR LA RUTA DE ALMACENAMIENTO
+        File ruta = Environment.getExternalStorageDirectory();
+        //DEFINIR EL NMBRE DE LA CARPETA
+        File NombreCarpeta = new File(ruta+"/WALLPAPERSAPP/");
+        NombreCarpeta.mkdir();
+        //DEFINIR EL NOMBRE DE LA IMAGEN DESCARGADA
+        String ObtenerNombreImagen = NombreImgenDetalle.getText().toString();
+        String NombreImagen = ObtenerNombreImagen +" "+ FechaDescarga + ".JPEG";
+        File file = new File(NombreCarpeta, NombreImagen);
+        OutputStream outputStream;
+        try {
+            outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            Toast.makeText(this, "La imagen se ha descargado con exito", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            //Log.e("FILE", e.getMessage());
+        }
+    }
+
+    private void DescargarImagen_11() {
+        bitmap = ((BitmapDrawable)ImagenDetalle.getDrawable()).getBitmap();
+        OutputStream fos;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+        String currentDate = sdf.format(new Date());
+
+        //Obtener el nombre de la imagen
+        String nombre_imagen = NombreImgenDetalle.getText().toString() + "_" + currentDate;
+
+        try {
+            ContentResolver resolver = getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, nombre_imagen);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "Image/jpeg");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES+ File.separator+"/WALLPAPERSAPP/");
+            Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+            if (imageUri != null) { // Check for null before proceeding
+                fos = resolver.openOutputStream(imageUri);
+                if (fos != null) {
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                    fos.close();
+                    Toast.makeText(this, "Imagen descargada con exito", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Image URI creation failed", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(this, "No se pudo descarar la imagen", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
